4744d12b921fa962ab1b0bf18ba5b2cfcf6fdb5f
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-22 19:18:55 +0200

    first commit

---
+++ components/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className='h-full flex flex-col items-center'>
+        <div className="flex">
+          <img src="/images/logo2.png" alt="logo" />
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
