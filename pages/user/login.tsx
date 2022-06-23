6d6b30a0ea4eedd03a12f4a53f61b9d035ca5576
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-23 22:59:43 +0200

    lat hurrah of thur

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -18,13 +18,13 @@ const Login = () => {
                 <label className='text-white' htmlFor='password'><BiLock className='text-xl' /></label>
                 <input className='w-full outline-none input bg-transparent ml-2' id='password' type='password' placeholder='Password' />
             </div>
-            <input className='w-full bg-gradient-to-r from-pink-500 to-violet-500 hover:bg-pink-600 duration-500 cursor-pointer h-[40px] mt-8  text-center text-lg flex items-center justify-center bg-pink-500 p-3 rounded-md'
+            <input className='w-full bg-gradient-to-r from-pink-500 to-violet-800 hover:bg-pink-600 duration-500 cursor-pointer h-[40px] mt-8  text-center text-lg flex items-center justify-center bg-pink-500 p-3 rounded-md'
              type="subimt" value="Login" />
 
              <div className="flex flex-col items-center">
                 <p className='text-xl my-2'>Or</p>
                 <button onClick={signInWithGoogle}
-                 className='bg-stone-800 p-1 h-[40px] w-full rounded-lg flex items-center justify-center'> <img className='max-h-full'
+                 className='bg-violet-700 p-1 h-[40px] w-full rounded-lg flex items-center justify-center'> <img className='max-h-full'
                  src="https://freesvg.org/img/1534129544.png" alt="" />
                     <p className='ml-2'>Continue with Google</p></button>
                     <div className="flex mt-4 items-center">
--- pages/user/signup.tsx
+++ pages/user/signup.tsx
@@ -18,22 +18,22 @@ const Signup = () => {
                 <label className='text-white' htmlFor='name'><BiUserCircle className='text-2xl' /></label>
                 <input className='w-full outline-none input text-lg bg-transparent ml-2' id='name' type='text' placeholder='Names' />
             </div>
-            <div className='border-2 border-stone-400 h-[40px] mt-4  flex  p-3 rounded-md items-center w-full'>
+            {/* <div className='border-2 border-stone-400 h-[40px] mt-4  flex  p-3 rounded-md items-center w-full'>
                 <label className='text-white' htmlFor='username'><BiUserCircle className='text-2xl' /></label>
                 <input className='w-full outline-none input text-lg bg-transparent ml-2' id='username' type='text' placeholder='username' />
-            </div>
+            </div> */}
             <div className='border-2 border-stone-400 h-[40px] mt-4  flex  p-3 rounded-md items-center w-full'>
                 <label className='text-white' htmlFor='password'><BiLock className='text-2xl' /></label>
                 <input className='w-full outline-none input text-lg bg-transparent ml-2' id='password' type='password' placeholder='Password' />
             </div>
             <input className='w-full cursor-pointer h-[40px] mt-4  text-center text-lg flex items-center duration-500
-             bg-gradient-to-r from-pink-500 to-violet-500 hover:bg-pink-600 justify-center bg-pink-500 p-3 rounded-md'
+             bg-gradient-to-r from-pink-500 to-violet-500 hover:bg-pink-800 justify-center bg-pink-500 p-3 rounded-md'
              type="subimt" value="Sign Up" />
 
              <div className="flex flex-col items-center">
                 <p className='text-xl my-2'>Or</p>
                 <button onClick={signInWithGoogle}
-                 className='bg-stone-800 p-1 h-[40px] w-full rounded-lg flex items-center justify-center'> <img className='max-h-full'
+                 className='bg-violet-700 p-1 h-[40px] w-full rounded-lg flex items-center justify-center'> <img className='max-h-full'
                  src="https://freesvg.org/img/1534129544.png" alt="" />
                     <p className='ml-2'>Continue with Google</p></button>
                     <div className="flex mt-4 items-center">
