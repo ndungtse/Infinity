dc6ab7ca8032364d234d3e08e203fea9684c6cb3
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-10-05 21:48:36 +0200

    feat: new environments and update packages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,78 @@
 import { url } from 'inspector'
 import React from 'react'
-import {FaFacebook, FaGithub, FaInstagram, FaInstagramSquare, FaLinkedin, FaLinkedinIn} from 'react-icons/fa'
+import { BiEnvelopeOpen, BiGlobe } from 'react-icons/bi'
+import {
+  FaFacebook,
+  FaFacebookSquare,
+  FaGithub,
+  FaGithubSquare,
+  FaInstagram,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaLinkedinIn,
+} from 'react-icons/fa'
 
 const Footer = () => {
-
-
-      const  git = 'https://github.com/NdungutseCharles103'
-      const  fa = ''
-      const  link = ''
-      const  ig = ''
-      const  email = ''
-   
+  const git = 'https://github.com/NdungutseCharles103'
+  const fa = ''
+  const link = ''
+  const ig = ''
+  const email = ''
 
   return (
-    <div className='w-full mt-11 flex items-center justify-center px-4'>
-        <img className='w-[50px]' src="/images/inlogo.png" alt="" />
-        <span>&copy; Charles Hazard 2022</span>
-        {/* <div className="flex items-center">
-            <a href={git} ref="noreferrer" className="" target="_blank">
-                <FaGithub />
-            </a>
-            <a href={git} ref="noreferrer" className="text-2xl" target="_blank">
-                <FaLinkedinIn />
-            </a>
-            <a href={git} ref="noreferrer" className="text-2xl" target="_blank">
-                <FaInstagram />
-            </a>
-            <a href={git} ref="noreferrer" className="text-2xl" target="_blank">
-                <FaFacebook />
-            </a>
-        </div> */}
+    <div className="mt-11 flex w-full items-center justify-center px-4">
+      <img className="w-[50px]" src="/images/inlogo.png" alt="" />
+      <span>&copy; Charles Hazard 2022</span>
+      <div className="flex items-center">
+        <a
+          className="flex items-center px-2"
+          href="https://www.linkedin.com/in/ishimwe-ndungutse-charles-079418227/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin className="text-xl" />
+        </a>
+        <a
+          className="flex items-center px-2"
+          href="https://github.com/NdungutseCharles103"
+          target="_blank"
+          rel="no-referrer noreferrer"
+        >
+          <FaGithubSquare className="text-xl" />
+        </a>
+        <a
+          className="flex items-center px-2"
+          href="https://www.facebook.com/ishimwendungutsecharles"
+          target="_blank"
+          rel="no-referrer noreferrer"
+        >
+          <FaFacebookSquare className="text-xl" />
+        </a>
+        <a
+          className="flex items-center px-2"
+          href="https://www.instagram.com/ndungutse_charles/"
+          target="_blank"
+          rel="no-referrer noreferrer"
+        >
+          <FaInstagramSquare className="text-xl" />
+        </a>
+        <a
+          className="flex items-center px-2"
+          href="mailto:ndungutsecharles103@gmail.com"
+          target="_blank"
+          rel="no-referrer noreferrer"
+        >
+          <BiEnvelopeOpen className="text-xl" />
+        </a>
+        <a
+          className="flex items-center px-2"
+          href="https://www.ndungutsecharles.me"
+          target="_blank"
+          rel="no-referrer noreferrer"
+        >
+          <BiGlobe className="text-xl" />
+        </a>
+      </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,8 +25,45 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/images/inlogo.png" />
-        <title>Infinity</title>
+        <link rel="icon" href="/images/inlogo.png" type="image/x-icon" />
+        <title>Infinity Gaming - Play Over 600,000 Games</title>
+        <meta
+          name="description"
+          content="Explore a vast collection of over 600,000 games and connect with other gamers on Infinity - the ultimate gaming destination."
+        />
+        <meta
+          name="keywords"
+          content="gaming, games, gamers, game collection, online gaming, multiplayer, gaming community"
+        />
+        <meta name="author" content="Ishimwe Ndungutse Charles" />
+        <meta
+          property="og:title"
+          content="Infinity Gaming - Play Over 600,000 Games"
+        />
+        <meta
+          property="og:description"
+          content="Join the ultimate gaming community! Discover and play over 600,000 games on Infinity. Connect with other gamers and have fun."
+        />
+        <meta
+          property="og:image"
+          content="/images/battlefield.jpg"
+        />
+        <meta property="og:url" content="https://infinity1.vercel.app" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Infinity Gaming - Play Over 600,000 Games"
+        />
+        <meta
+          name="twitter:description"
+          content="Join the ultimate gaming community! Discover and play over 600,000 games on Infinity. Connect with other gamers and have fun."
+        />
+        <meta
+          name="twitter:image"
+          content="/images/battlefield.jpg"
+        />
       </Head>
       <AppProvider>
         <PostProvider>
